601dc59e88af1c192a1b9907111c38773f213f4b
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2025-01-28 13:41:25 +0330

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'Blog.apps.BlogConfig',
+    'Food.apps.FoodConfig',
+    'User.apps.UserConfig',
+
 ]
 
 MIDDLEWARE = [
