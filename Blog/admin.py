5f3c8303386dd2d2465ca3880558cd6078977380
AliReza Doostmohammadi <alireza.dm80@gmail.com>
2025-01-28 13:37:23 +0330

    Update Blog/models.py

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug']
+    search_fields = ['name']
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'category_name', 'thumbnail_tag', 'created', 'updated']
+    readonly_fields = ('thumbnail_tag',)
+    search_fields = ['title', 'category_name']
+    prepopulated_fields = {'slug':('title',)}
+
+
+    #to display category name
+    def category_name(self, obj):
+        return obj.category.name
+    category_name.short_description = 'Category'
--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.utils import html 
 
-# Create your models here.
+
+# Manager
+class CategoryQuerySet(models.QuerySet):
+    def active(self):
+        return self.filter(status=True)
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=50)
+    status = models.BooleanField(default=True)
+
+    # initializing the manager
+    objects = CategoryQuerySet.as_manager()
+  
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+
+    # initializing the manager
+    objects = CategoryQuerySet.as_manager()
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=50)
+    category = models.ForeignKey(Category, related_name='article_category', on_delete=models.CASCADE)
+    description = models.TextField()
+    status = models.BooleanField(default = True)
+    thumbnail = models.ImageField(upload_to='media/blog/')   
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def thumbnail_tag(self):
+        #checks for the existence of thumbnail before hand
+        if self.thumbnail:
+            return html.format_html("<img width=100; height=100, style='border-radius: 10px;' \
+                                                    src='{}'>", self.thumbnail.url)
+        return 'No Image'
+    
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+        ordering = ["created"]
